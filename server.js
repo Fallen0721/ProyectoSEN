fd60a9ce7a6b4cf4537b2d4ac52d364b8f27efab
1Armagedon0 <144762539+1Armagedon0@users.noreply.github.com>
2024-08-09 06:08:16 -0600

    Update server.js

--- server.js
+++ server.js
@@ -17,3 +17,25 @@ const db = mysql.createConnection({
   password: 'Armagedon97',
   database: 'bdecoenergy'
 });
+
+db.connect((err) => {
+    if (err) {
+      console.error('Error connecting to the database:', err);
+      return;
+    }
+    console.log('Connected to the database');
+  });
+
+
+  app.post('/register', (req, res) => {
+    const { name, email, password, lastname, birthdate, phone, city, department, sex, Acceptterms} = req.body;
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    const sql = 'INSERT INTO users (name, email, password, lastname, birthdate, phone, city, department, sex, acceptterms) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+    db.query(sql, [name, email, hashedPassword], (err, result) => {
+      if (err) {
+        res.status(500).send('Error registering user');
+        return;
+      }
+      res.status(200).send('User registered successfully');
+    });
+  });
