2fa35bb5be6f80228a976d2fdbcee93f968761ee
1Armagedon0 <144762539+1Armagedon0@users.noreply.github.com>
2024-08-09 06:00:02 -0600

    Crud

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Armagedon97',
+  database: 'bdecoenergy'
+});
